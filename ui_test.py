1f2948b264005082d4186c326d0f8721e9c6280b
William Gyrulf <william.gyrulf@hotmail.com>
2026-04-15 10:39:29 +0200

    Fix

--- ui_test.py
+++ ui_test.py
@@ -1,2 +1,13 @@
-import pyqt5
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5 import uic
 
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi("Main.ui", self)
+
+app = QApplication(sys.argv)
+window = MainWindow()
+window.show()
+sys.exit(app.exec_())
