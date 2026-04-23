6844e33c53a865e71fbeb93bc17b04c9aa092234
William Gyrulf <william.gyrulf@hotmail.com>
2026-04-23 11:06:33 +0200

    Created video player

--- ui_test.py
+++ ui_test.py
@@ -1,11 +1,37 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow
+import os
+from PyQt5.QtWidgets import QApplication, QMainWindow, QVBoxLayout
+from PyQt5.QtMultimedia import QMediaPlayer, QMediaContent
+from PyQt5.QtMultimediaWidgets import QVideoWidget
+from PyQt5.QtCore import QUrl
 from PyQt5 import uic
+
+def on_render_pressed(selection):
+    print(f"render button pressed {selection}")
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         uic.loadUi("Main.ui", self)
+
+        self.video_widget = QVideoWidget(self.videoFrame)
+        layout = QVBoxLayout(self.videoFrame)
+        layout.setContentsMargins(0, 0, 0, 0)
+        layout.addWidget(self.video_widget)
+
+        self.player = QMediaPlayer(self)
+        self.player.setVideoOutput(self.video_widget)
+
+        video_path = os.path.abspath("output.mp4")
+        self.player.setMedia(QMediaContent(QUrl.fromLocalFile(video_path)))
+
+        self.playButton.clicked.connect(self.player.play)
+        self.stopButton.clicked.connect(self.player.stop)
+        self.pushButton.clicked.connect(self.on_render)
+        self.quitButton.clicked.connect(app.quit)
+
+    def on_render(self):
+        on_render_pressed(self.comboBox.currentText())
 
 app = QApplication(sys.argv)
 window = MainWindow()
